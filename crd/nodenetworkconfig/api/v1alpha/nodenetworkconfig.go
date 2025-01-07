82f93b534091b9b222138a8222eb8b8a45393951
Ashish Nair <nairashu@gmail.com>
2025-01-07 14:39:26 -0800

    fix: Updating the AvailabilityZone field in the NNC to be an int value as the return from NMA is an int value (#3331)
    * fix: Updating the AvailabilityZone field in the NNC to be a int value as the return from NMA is an int value
    
    * fix: Updated the maifest as well
    
    * Keeping the version as uint to avoid casting it multiple times and DNC has NCGoalstate as uint
    
    ---------
    
    Co-authored-by: Ashish Nair <ashish.nair@microsoft.com>

--- crd/nodenetworkconfig/api/v1alpha/nodenetworkconfig.go
+++ crd/nodenetworkconfig/api/v1alpha/nodenetworkconfig.go
@@ -49,7 +49,7 @@ type NodeNetworkConfigSpec struct {
 	IPsNotInUse      []string `json:"ipsNotInUse,omitempty"`
 	// AvailabilityZone contains the Azure availability zone for the virtual machine where network containers are placed.
 	// +kubebuilder:validation:Optional
-	AvailabilityZone string `json:"availabilityZone,omitempty"`
+	AvailabilityZone uint `json:"availabilityZone,omitempty"`
 }
 
 // Status indicates the NNC reconcile status
